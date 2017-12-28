547b9cbc8aa2a64ee47716624384d0fcda607181
Patrick Brock <patrick.brock@evverbank.com>
2017-12-28 13:50:30 -0500

    fixes objects getting nested further into array than they should when creating todos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -61,8 +61,11 @@ export class AppComponent implements OnInit {
     console.log(this.newTodo);
     this.todoService.createTodo(this.newTodo)
       .subscribe((res) => {
+        console.log('result');
+        console.log(res.data);
+        console.log('------');
         console.log(this.todosList);
-        this.todosList.push(res.data);
+        this.todosList = res.data;
         this.newTodo = new ToDo();
       });
   }
